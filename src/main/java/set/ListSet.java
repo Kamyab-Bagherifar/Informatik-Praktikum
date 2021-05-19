60ee0c58902bb6831c6ae56ce86f50d616426211
322160 <bagherifar.kamyab@stud.th-luebeck.de>
2021-05-19 19:57:18 +0200

    ListSet_update_1

--- src/main/java/set/ListSet.java
+++ src/main/java/set/ListSet.java
@@ -5,27 +5,73 @@ import list.List;
 
 
 public class ListSet<A> implements Set<A> {
+    private List<A> set;
 
-	private ListSet(){
-	}
+    //Kontruktor
+    private ListSet() {
+        set = List.list();
+    }
 
-  public static <A> Set<A> empty() {
-    return new ListSet<>();
-  }
-  
-  public static <A> Set<A> fromList(List<A> list) {
-		return new ListSet<>();
-  }
+    private ListSet(List<A> l) {
+        set = l;
+    }
 
-  @SafeVarargs
-  public static <A> Set<A> set(A... as) {
-		return new ListSet<>();
-  }
+    // Fabrik Methoden
+    public static <A> Set<A> empty() {
+        return new ListSet<>();
+    }
 
-  public boolean isEqualTo(Set<A> other){
-	  return false;
-  }
+    public static <A> Set<A> fromList(List<A> list) {
+        return new ListSet<>(list);
+    }
 
+    @SafeVarargs
+    public static <A> Set<A> set(A... as) {
+        return new ListSet<>(List.list(as));
+    }
 
+    public boolean isEqualTo(Set<A> other) {
+        return set.isEqualTo(other.toList());
+    }
 
+    @Override
+    public Set<A> insert(A e) {
+        return set.elem(e) ? fromList(set.map(x -> x == e ? e: x)) : fromList(set.cons(e));
+
+    }
+
+    @Override
+    public Set<A> delete(A e) {
+        return fromList(set.delete(e));
+    }
+
+    @Override
+    public boolean member(A e) {
+        return  set.elem(e);
+    }
+
+    @Override
+    public int size() {
+        return set.length();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    @Override
+    public A findEq(A e) {
+        return set.elem(e) ? e : null;
+    }
+
+    @Override
+    public List<A> toList() {
+        return set;
+    }
+
+    @Override
+    public String toString() {
+        return set.toString();
+    }
 }
--- src/main/java/set/Set.java
+++ src/main/java/set/Set.java
@@ -7,5 +7,13 @@ import list.List;
 public interface Set<A>  {
 
   boolean isEqualTo(Set<A> other);
+    Set<A> insert(A e);
+    Set<A> delete(A e);
+    boolean member(A e);
+    int size();
+    boolean isEmpty();
+    A findEq(A e);
+    List<A> toList();
+
 
 }
