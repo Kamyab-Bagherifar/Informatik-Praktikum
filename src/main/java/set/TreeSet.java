82e8503f54bdeb1de3f5300feac2ad2bba800380
322160 <bagherifar.kamyab@stud.th-luebeck.de>
2021-06-27 22:10:52 +0200

    SortedSet_finished

--- src/main/java/set/TreeSet.java
+++ src/main/java/set/TreeSet.java
@@ -46,87 +46,87 @@ public class TreeSet<A extends Comparable<A>> implements SortedSet<A> {
     @Override
     public Set<A> insert(A e) {
         return tree.insert(e).inorder().toSet();
-    }
+    } //O(n)
 
     @Override
     public Set<A> delete(A e) {
         return tree.delete(e).inorder().toSet();
-    }
+    }//O(n)
 
     @Override
     public boolean member(A e) {
         return tree.member(e);
-    }
+    }//O(n)
 
     @Override
     public int size() {
         return tree.size();
-    }
+    }//O(n)
 
     @Override
     public boolean isEmpty() {
         return tree.isEmpty();
-    }
+    }//O(1)
 
     @Override
     public A findEq(A e) {
         return tree.findEq(e);
-    }
+    }//O(n)
 
     @Override
     public List<A> toList() {
         return tree.inorder();
-    }
+    }//O(n)
 
     @Override
     public boolean any(Function<A, Boolean> p) {
         return tree.inorder().any(p);
-    }
+    }//O(2n)
 
     @Override
     public boolean all(Function<A, Boolean> p) {
         return tree.inorder().all(p);
-    }
+    }//O(2n)
 
     @Override
     public boolean isSubsetOf(Set<A> s) {
         return tree.inorder().toSet().isSubsetOf(s);
-    }
+    }//O(2n)
 
     @Override
     public boolean disjoint(Set<A> s) {
         return tree.inorder().toSet().disjoint(s);
-    }
+    }//O(2n)
 
     @Override
     public Set<A> union(Set<A> s) {
         return tree.inorder().toSet().union(s);
-    }
+    }//O(2n)
 
     @Override
     public Set<A> intersection(Set<A> s) {
         return tree.inorder().toSet().intersection(s);
-    }
+    }//O(2n)
 
     @Override
     public <B> B foldr(Function<A, Function<B, B>> f, B s, Set<A> xs) {
         return tree.inorder().toSet().foldr(f, s, xs);
-    }
+    }//O(2n)
 
     @Override
     public <B> B foldl(Function<B, Function<A, B>> f, B s, Set<A> xs) {
         return tree.inorder().toSet().foldl(f, s, xs);
-    }
+    }//O(2n)
 
     @Override
     public Set<A> filter(Function<A, Boolean> f, Set<A> xs) {
         return tree.inorder().toSet().filter(f, xs);
-    }
+    }//O(2n)
 
     @Override
     public <B> Set<B> map(Function<A, B> f, Set<A> xs) {
         return tree.inorder().toSet().map(f, xs);
-    }
+    }//O(2n)
 
     @Override
     public Result<A> lookupEq(A x) {
